f43a10c23e5aa74a0079803a0745dfdbde6503a8
chernyaevea <chernyaevea@gmail.com>
2017-02-26 22:56:39 +0300

    add some validation & cancel button

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import PostReducer from './reducer_posts';
-import {reducer as formReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   posts: PostReducer,
